5291ba06cb57bffbb04b3c6d1511f56d6002ed8c
adipopax <adipopa987@gmail.com>
2024-02-10 22:41:15 +0200

    fresh start lets see how it goes :)

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
-  <header>
-    <div className='advertising first-advertising'>
-      Order over £50 have free shipping
-    </div>
-  </header>
+    <header>
+      <div className='advertising first-advertising'>
+        Order over £50 have free shipping
+      </div>
+      <nav className='nav'>
+        <img src="/imagini/logo_2.png" alt="logo" className='logo'/>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </nav>
+    </header>
   );
 }
 
