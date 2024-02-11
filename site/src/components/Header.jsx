f2ed6ddd57b4a9e733e64041d9c982da2ce52718
adipopax <adipopa987@gmail.com>
2024-02-11 17:33:44 +0200

    .

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Header.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { BsCart2 } from "react-icons/bs"; 
+import { FaRegHeart } from "react-icons/fa"; 
+import { MdAccountBox } from "react-icons/md";
 
 const Header = () => {
   return (
@@ -11,7 +13,19 @@ const Header = () => {
       </div>
       <nav className='nav'>
         <img src="/imagini/logo_2.png" alt="logo" className='logo'/>
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <form action="" className='search-container'>
+          <input type='text' placeholder='Search...' className='search-input'/>
+          <button type='submit' className='search-button'><HiMagnifyingGlass className='mglass'/></button>
+        </form>
+        <div className='buttons'>
+          <button className='btn acc-btn'><MdAccountBox /></button>
+        </div>
+        <div className='buttons'>
+          <button className='btn fav-btn'><FaRegHeart /></button>
+        </div>
+        <div className='buttons'>
+          <button className='btn cart-btn'><BsCart2 /></button>
+        </div>
       </nav>
     </header>
   );
