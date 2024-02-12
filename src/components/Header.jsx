d3c4949c7b78c0bb6243c470c0d6d08526c972e0
adipopax <adipopa987@gmail.com>
2024-02-12 19:10:26 +0200

    added new language dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Header.css';
-import { HiMagnifyingGlass } from "react-icons/hi2";
-import { BsCart2 } from "react-icons/bs"; 
-import { FaRegHeart } from "react-icons/fa"; 
-import { MdAccountBox } from "react-icons/md";
+import { HiOutlineHeart } from "react-icons/hi";
+import { MdPerson } from "react-icons/md";
+import { IoCartOutline } from "react-icons/io5";
+import { HiOutlineGlobeAlt } from "react-icons/hi";
+import { MdArrowDropDown } from "react-icons/md";
 
 const Header = () => {
   return (
@@ -12,20 +13,29 @@ const Header = () => {
         Order over £50 have free shipping
       </div>
       <nav className='nav'>
-        <img src="/imagini/logo_2.png" alt="logo" className='logo'/>
+        <div className='logo-container'>
+            <img src="/imagini/logo_2.png" alt="logo" className='logo'/>
+            <div className="dropdown">
+              <button className="dropdown-toggle">
+                <HiOutlineGlobeAlt />
+                <MdArrowDropDown />
+              </button>
+              <div className="dropdown-menu">
+                <a href="#">English</a>
+                <a href="#">Spanish</a>
+                <a href="#">Romanian</a>
+              </div>
+            </div>
+          </div>
+        <div className='buttons'>
+          <button className='btn acc-btn'><MdPerson /></button>
+          <button className='btn fav-btn'><HiOutlineHeart /></button>
+          <button className='btn cart-btn'><IoCartOutline /></button>
+        </div>
         <form action="" className='search-container'>
           <input type='text' placeholder='Search...' className='search-input'/>
-          <button type='submit' className='search-button'><HiMagnifyingGlass className='mglass'/></button>
+          <button type='submit' className='search-button'>Search</button>
         </form>
-        <div className='buttons'>
-          <button className='btn acc-btn'><MdAccountBox /></button>
-        </div>
-        <div className='buttons'>
-          <button className='btn fav-btn'><FaRegHeart /></button>
-        </div>
-        <div className='buttons'>
-          <button className='btn cart-btn'><BsCart2 /></button>
-        </div>
       </nav>
     </header>
   );
